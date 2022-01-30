0b13b9b679a2c4ba4c0139e77d34235e8629db8b
Rob <93198775+robdrawspictures@users.noreply.github.com>
2022-01-30 17:57:14 +0000

    Added content and a feature to the home page.

--- forum_ipsem/app.py
+++ forum_ipsem/app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 from controllers.post_controller import posts_blueprint
 from controllers.thread_controller import threads_blueprint
 from controllers.user_controller import users_blueprint
+import repositories.user_repository as user_repository
+import repositories.post_repository as post_repository
+import repositories.thread_repository as thread_repository
+import random
 
 app = Flask(__name__)
 
@@ -12,7 +16,12 @@ app.register_blueprint(users_blueprint)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    quotes = ["ayy lmao", "the wages of sin are death", "safety not guaranteed", "Stay hydrated. This is a threat"]
+    quote = random.choice(quotes)
+    users = user_repository.select_all()
+    posts = post_repository.select_all()
+    threads = thread_repository.select_all()
+    return render_template('index.html', quote = quote, users = users, threads = threads, posts = posts)
 
 if __name__ == '__main__':
     app.run(debug=True)

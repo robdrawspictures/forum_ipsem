adc3f5cfe4870cbdbfa55a30b9eca24e888c869c
Rob <93198775+robdrawspictures@users.noreply.github.com>
2022-01-31 12:04:02 +0000

    Posts can now be added to threads, but redirect is broken.

--- forum_ipsem/controllers/thread_controller.py
+++ forum_ipsem/controllers/thread_controller.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.user import User
 from models.thread import Thread
+from models.post import Post
 import repositories.user_repository as user_repository
 import repositories.thread_repository as thread_repository
+import repositories.post_repository as post_repository
 
 threads_blueprint = Blueprint("threads", __name__)
 
@@ -20,6 +22,17 @@ def show(id):
     posts = thread_repository.get_posts(id)
     users = user_repository.select_all()
     return render_template("threads/thread.html", thread = thread, posts = posts, users = users)
+
+@threads_blueprint.route("/threads/<id>", methods=['POST'])
+def new_post(id):
+    user_id = int(request.form['user_id'])
+    post_content = request.form['post_content']
+    thread_id = id
+    new_post = Post(post_content, user_id, thread_id)
+    post_repository.create_post(new_post)
+    return redirect("/threads/<id>")
+
+
 
 # Note for future self: You're going to need a find_user_by_name function to
 # get around this fine mess you've made, using the creator's name instead of

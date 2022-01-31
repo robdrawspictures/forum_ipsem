529ec143116c5c0221b1177261ed97ad8e28ec11
Rob <93198775+robdrawspictures@users.noreply.github.com>
2022-01-31 11:24:04 +0000

    All posts by user now appear on user account page, with link to relevant thread.

--- forum_ipsem/controllers/user_controller.py
+++ forum_ipsem/controllers/user_controller.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.user import User
 import repositories.user_repository as user_repository
+import repositories.post_repository as post_repository
+import repositories.thread_repository as thread_repository
 
 users_blueprint = Blueprint("users", __name__)
 
@@ -14,7 +16,18 @@ def users():
 def new_user():
     return render_template("/users/new.html")
 
+@users_blueprint.route("/users", methods=["POST"])
+def add_user():
+    user_name = request.form["user_name"]
+    avatar_id = request.form["avatar_id"]
+    admin_status = request.form["admin_status"]
+    user = User(user_name, avatar_id, False, admin_status)
+    user_repository.create_user(user)
+    return redirect("/users")
+
 @users_blueprint.route("/users/<id>")
 def show_user(id):
     user = user_repository.select(id)
-    return render_template("users/user.html", user = user)
+    posts = post_repository.select_posts_by_user(id)
+    threads = thread_repository.select_all()
+    return render_template("users/user.html", user = user, posts = posts, threads = threads)

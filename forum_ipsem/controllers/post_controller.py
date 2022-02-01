ad04795a2c1b2cefc864a219fd0d3609b5f0cfcc
Rob <93198775+robdrawspictures@users.noreply.github.com>
2022-02-01 14:04:12 +0000

    Posts can now be edited.

--- forum_ipsem/controllers/post_controller.py
+++ forum_ipsem/controllers/post_controller.py
@@ -23,6 +23,18 @@ def show(id):
     users = user_repository.select_all()
     return render_template("posts/post.html", post = post, users = users)
 
+@posts_blueprint.route("/posts/<id>/edit", methods=['GET'])
+def edit_post(id):
+    post = post_repository.select(id)
+    return render_template("/posts/edit.html", post = post)
+
+@posts_blueprint.route("/posts/<id>/edit", methods=['POST'])
+def post_edited(id):
+    edit = request.form['post_edit']
+    post_repository.edit_post(edit, id)
+    post = post_repository.select(id)
+    return redirect("/threads/" + str(post.thread_id))
+
 @posts_blueprint.route("/posts/<id>/delete", methods=['POST'])
 def post_deleted(id):
     thread = post_repository.select(id)
